df66772481df53ea19433d0e549b1f2271bd445c
All Fine Maulinaro <allfinem10@gmail.com>
2025-01-16 10:29:46 +0700

    delete component and setup axios for get user

---
+++ src/components/ListUsers.vue
@@ -0,0 +1,25 @@
+<script setup>
+import axios from "axios";
+import { onMounted, reactive } from "vue";
+
+const state = reactive({
+  users: {},
+  isLoading: true,
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/users`
+    );
+    state.users = response.data;
+    console.log(JSON.stringify(state.users));
+  } catch (error) {
+    console.error("Error fetching jobs", error);
+  } finally {
+    state.isLoading = false;
+  }
+});
+</script>
+
+<template></template>
